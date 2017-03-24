25bb0a225c38c97f5c924cb4cf0b733bfa39fc75
William Williams <bigwilliestyle@gmail.com>
2017-03-23 23:20:12 -0700

    Add scripts for doing deploy and running integration tests

--- backend/oracle.py
+++ backend/oracle.py
@@ -10,5 +10,5 @@ status_to_orders = {
 def handler(event, context):
     # print("Received Status: \n" + json.dumps(event, indent=2))
     status = event['status']
-    print("Received Status: " + str(status))
+    # print("Received Status: " + str(status))
     return json.dumps({'orders': status_to_orders[status]})
