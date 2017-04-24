8de33841e65c236e72129619e2495869b421425e
William Williams <bigwilliestyle@gmail.com>
2017-04-24 10:04:55 -0700

    Clean up how we select which database to use

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,8 +1,11 @@
+import os
 from cassandradb import CassandraDB
 from dynamodb import DynamoDB
 
 
 def get_db(name='dynamodb'):
+    name = os.getenv('T_DATABASE', name)
+
     if name == 'dynamodb':
         return DynamoDB()
     elif name == 'cassandra':
